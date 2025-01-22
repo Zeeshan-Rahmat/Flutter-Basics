e722044271793815ef96a15193ed78f5be80763d
Zeeshan Rahmat <zeeshanullah.freelancer@gmail.com>
2025-01-22 10:50:54 +0500

    Day 29 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,19 +3,21 @@ import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 import 'dart:convert';
 import "package:flutter_basics/models/catalog.dart";
+import "package:flutter_basics/pages/main.dart";
 import "package:flutter_basics/utils/my_routes.dart";
 import "package:flutter_basics/widgets/custom_drawer.dart";
 import "package:flutter_basics/widgets/item_widget.dart";
 import "package:flutter_basics/widgets/my_theme.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 
-class HomePage extends StatefulWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  ConsumerState<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends ConsumerState<HomePage> {
   @override
   void initState() {
     super.initState();
@@ -37,22 +39,43 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final int itemCount = ref.watch(cartProvider).items.length;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Basics'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, MyRoutes.cartRoute).then((value) {
-            if (value == true) {
-              setState(() {});
-            }
-          });
-        },
-        child: Icon(
-          CupertinoIcons.cart,
-          color: MyTheme.dark_brightColor,
-        ),
+      floatingActionButton: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.pushNamed(context, MyRoutes.cartRoute).then((value) {
+                if (value == true) {
+                  setState(() {});
+                }
+              });
+            },
+            child: Icon(
+              CupertinoIcons.cart,
+              color: MyTheme.dark_brightColor,
+            ),
+          ),
+          Positioned(
+            top: -8,
+            right: -5,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+              child: Text(
+                itemCount.toString(),
+                style: const TextStyle(fontSize: 12, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
